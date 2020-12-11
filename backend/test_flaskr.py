2335601cfa2fdb0f064afba087b5de8f2659cf2c
Omar Sakr <mero.saker@gmil.com>
2020-12-12 00:35:11 +0200

    Add tests for /questions endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -3,8 +3,8 @@ import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
-from .flaskr import create_app
-from .models import setup_db, Question, Category
+from flaskr import create_app
+from models import setup_db, Question, Category
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = database_path = "postgres://{}:{}@{}/{}".format('omar', 'reomar15', 'localhost:5432', self.database_name)
+
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,15 +25,38 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
+
 
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_all_questions(self):
+        """Test /questions endpoint that retrives all the questions """
+
+        res = self.client().get('/questions?page=2')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertNotEqual(data['total_questions'], 0)
+        self.assertTrue(data['categories'])
+
+    def test_404_if_questions_dosent_exist(self):
+        """Test /questions endpoint when page dosen't exist (404) """
+
+        res = self.client().get('/questions?page=10000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not found')
+
 
 
 # Make the tests conveniently executable
